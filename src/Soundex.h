d398f530d8d9ce43d7a244b51f7e3a1b7317a1ec
superxc <superxcgm@gmail.com>
2020-05-03 12:56:15 +0800

    rule2 0.3 refactor

--- src/Soundex.h
+++ src/Soundex.h
@@ -8,16 +8,29 @@
 class Soundex {
 
 public:
+
     std::string encode(const std::string &word) const {
-        auto encoded = word.substr(0, 1);
-        if (word.length() > 1)
-            encoded += "1";
-        return zeroPad(encoded);
+        return zeroPad(head(word) + encodedDigits(word));
     }
 
 private:
+    std::string encodedDigits(const std::string &word) const {
+        if (word.length() > 1) return encodedDigit();
+        return "";
+    }
+
+    std::string head(const std::string &word) const {
+        return word.substr(0, 1);
+    }
+
+    std::string encodedDigit() const {
+        return "1";
+    }
+
+    static const size_t MaxCodeLength{4};
+
     std::string zeroPad(const std::string &word) const {
-        auto zerosNeeded = 4 - word.length();
+        auto zerosNeeded = MaxCodeLength - word.length();
         return word + std::string(zerosNeeded, '0');
     }
 };
